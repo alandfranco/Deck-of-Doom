8dd7120a5e6afc50b4d5f818ae677e3900186e4d
alandfranco <33072356+alandfranco@users.noreply.github.com>
2021-03-02 00:51:10 -0300

    Necro ON Fungus

---
+++ Assets/Scripts/Potions/PotionSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Potion", menuName = "Potions/New Potion")]
+public class PotionSO : ScriptableObject
+{
+    public new string name;    
+}
